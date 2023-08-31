1321dec2ae8c0a119a09f0bf03bb890c0c10aad9
cltubigon <60680621+cltubigon@users.noreply.github.com>
2023-09-01 02:58:59 +0800

    test3

--- src/Components/home/HomeSectionOne.js
+++ src/Components/home/HomeSectionOne.js
@@ -52,7 +52,7 @@ function SectionOne() {
             pr={{ base: 0, lg: "20px" }}
             lineHeight={"1.05em"}
           >
-            Grow with great designss
+            Grow with great design
           </Heading>
           <Text pl={{ base: "0", lg: "30px" }} pr={{ base: "0", lg: "50px" }}>
             No matter what your business needs, we can connect you with a
